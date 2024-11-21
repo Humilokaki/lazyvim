3d2e71b8f2f81104584cfc5bab662f702c9fe75c
Aidan G <Aidan.Gallagher@insa-rennes.fr>
2024-11-21 17:27:44 +0100

    Add n, <leader>cp to create a python block in md files

--- ftplugin/markdown.lua
+++ ftplugin/markdown.lua
@@ -1,1 +1,9 @@
 require("quarto").activate()
+
+vim.keymap.set("n", "<leader>cp", function()
+  local line = vim.fn.line(".")
+  vim.fn.append(line, "```python")
+  vim.fn.append(line + 1, "")
+  vim.fn.append(line + 2, "```")
+  vim.cmd("normal! j")
+end, { buffer = true, desc = "Insert Python Code Block" })
